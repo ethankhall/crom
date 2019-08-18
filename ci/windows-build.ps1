e425aa2387a88bf207ad0853d37857b6d8d8553a
Ethan Hall <ethan@ehdev.io>
2019-08-17 22:08:24 -0700

    Trying new Windows Build Steps

--- ci/windows-build.ps1
+++ ci/windows-build.ps1
@@ -1,8 +1,8 @@
 invoke-restmethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
 & ./rustup-init.exe -y --default-toolchain nightly-x86_64-pc-windows-msvc --no-modify-path
-& %USERPROFILE%\.cargo\bin\rustup.exe install nightly-x86_64-pc-windows-msvc
+%USERPROFILE%\.cargo\bin\rustup.exe install nightly-x86_64-pc-windows-msvc
 remove-item rustup-init.exe
-& %USERPROFILE%\.cargo\bin\cargo.exe +nightly-x86_64-pc-windows-msvc build --release
+%USERPROFILE%\.cargo\bin\cargo.exe +nightly-x86_64-pc-windows-msvc build --release
 .\target\release\crom.exe update-version --pre-release release
-& %USERPROFILE%\.cargo\bin\cargo.exe +nightly-x86_64-pc-windows-msvc build --release
-& %USERPROFILE%\.cargo\bin\cargo.exe +nightly-x86_64-pc-windows-msvc test --release
+%USERPROFILE%\.cargo\bin\cargo.exe +nightly-x86_64-pc-windows-msvc build --release
+%USERPROFILE%\.cargo\bin\cargo.exe +nightly-x86_64-pc-windows-msvc test --release
