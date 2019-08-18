b44335a2cd324cf6274d24dc7752a7bdaa228c09
Ethan Hall <ethan@ehdev.io>
2019-08-17 21:47:17 -0700

    Update windows install

--- ci/windows-build.ps1
+++ ci/windows-build.ps1
@@ -1,8 +1,8 @@
 invoke-restmethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
 & ./rustup-init.exe -y --default-toolchain nightly-x86_64-pc-windows-msvc --no-modify-path
-& "C:/Program Files/Rust/.cargo/bin/rustup.exe" install nightly-x86_64-pc-windows-msvc
+& "%userprofile%/.cargo/bin/rustup.exe" install nightly-x86_64-pc-windows-msvc
 remove-item rustup-init.exe
-& "C:/Program Files/Rust/.cargo/bin/cargo.exe" +nightly-x86_64-pc-windows-msvc build --release
+& "%userprofile%/.cargo/bin/cargo.exe" +nightly-x86_64-pc-windows-msvc build --release
 .\target\release\crom.exe update-version --pre-release release
-& "C:/Program Files/Rust/.cargo/bin/cargo.exe" +nightly-x86_64-pc-windows-msvc build --release
-& "C:/Program Files/Rust/.cargo/bin/cargo.exe" +nightly-x86_64-pc-windows-msvc test --release
+& "%userprofile%/.cargo/bin/cargo.exe" +nightly-x86_64-pc-windows-msvc build --release
+& "%userprofile%/.cargo/bin/cargo.exe" +nightly-x86_64-pc-windows-msvc test --release
